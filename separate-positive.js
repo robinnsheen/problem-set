d0bb76dacd876170e9403492027c40cf492dff58
Ian Dao <ibdao921@gmail.com>
2022-05-27 16:15:18 -0700

    separate+ done

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,24 @@
+"use-strict"
 // add whatever parameters you deem necessary & write docstring
-function separatePositive() {
+/** Given an array of non-zero numbers
+ * separate the positives to the left, negatives to the right
+ * and return the same array with the numbers " +/- sorted"
+ * must be done in place.
+ */
+function separatePositive(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while(left < right){
+        if (nums[left] < 0){
+            if (nums[right] < 0){
+                right--; 
+            }else{
+                [nums[left], nums[right]] = [nums[right], nums[left]]
+            }   
+        }else{
+            left++;
+        }
+    }
+    return nums;
 }
