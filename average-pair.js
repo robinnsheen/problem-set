6e25b6074b92beabc530db12583581cb526c4aca
Ian Dao <ibdao921@gmail.com>
2022-05-27 15:18:05 -0700

    avg pair done

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,23 @@
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+/** takes a sorted array of numbers (nums), numeric value (targetAvg)
+ * to see if there is a pair where the average = target (true or false)
+ * there can be more than one pair. 
+ */
+function averagePair(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let avg = (nums[left] + nums[right]) / 2;
+        if (avg === target) {
+            return true;
+        } else if (avg > target) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+    return false;
+
 }
 
