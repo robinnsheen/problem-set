199ecf786960b2f23b2a93adee21f450bc7cb360
robin <robinnsheen@berkeley.edu>
2022-05-27 15:33:49 -0700

    two-arr-obj

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,28 @@
 // add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+/**
+ * input: 2 arrays, possibly diff lenghts
+ * 1st arr: keys
+ * 2nd: values
+ * return: obj created from keys and values
+ *
+ * not enough val: rest of keys have val of null
+ * not enough keys: ignore keys
+ */
+function twoArrayObject(keyArr, valArr) {
+
+  let obj = {};
+
+  for (let i=0; i < keyArr.length; i++) {
+
+    if (valArr[i] === undefined) {
+      obj[keyArr[i]] = null;
+    } else {
+      obj[keyArr[i]] = valArr[i];
+    }
+
+
+  }
+
+  return obj;
+
 }
