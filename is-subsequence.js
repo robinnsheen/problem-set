806eee7266714290c8121742745ceab28cd76848
Ian Dao <ibdao921@gmail.com>
2022-05-27 17:22:26 -0700

    is-subsequence needs a fail fast fn

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,90 @@
+"use strict";
 // add whatever parameters you deem necessary & write docstring
-function isSubsequence() {
+/** Given two strings, s1 and s2
+ * if s1 can be found in s2 as a subsequence (order of the letter matters)
+ * return true or false
+ */
+
+function isSubsequence(s1, s2) {
+    let i = 0;
+    let j = 0;
+    let match;
+    //case 1
+    while (i < s1.length || j < s2.length){
+        if (s1[i] !== s2[j]){
+            j++;
+            match = false;
+        }else{
+            i++;
+            j++;
+        }
+        
+        
+        if (i === s1.length && s1[i] !== s2[j]){
+            j++;
+            match = false;
+        }else{
+            match = true;
+        }
+
+    }
+    return match;
+   
+    /*
+    //case 2
+    while (i === s1.length - 1 && j < s2.length){
+        if (s1[i] !== s2[j]){
+            j++;
+        }else return true;
+        return false;
+    }
+
+    // case 3
+    if (i < s1.length && j === s2.length - 1) return false */ 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getCounts(items){
+    const freq = {};
+    for (let item of items){
+        const curr = freq[item] || 0;
+        freq[item] = curr + 1;
+    }
 }
