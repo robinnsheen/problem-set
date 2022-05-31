983064c04166e6d853b82d85ddfa09a62e3b6bcf
robin <robinnsheen@berkeley.edu>
2022-05-31 00:56:34 -0700

    finished countPairs

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,19 @@
-// add whatever parameters you deem necessary & write docstring
-function countPairs() {
+"use strict";
+
+/*count the number of pairs that add to target sum */
+function countPairs(ints, num) {
+  const sumPairs = new Set();
+  let pairCount = 0;
+
+  for (let int of ints) {
+    const diff = num-int;
+    if (sumPairs.has(diff)) {
+      pairCount ++;
+    } else {
+      sumPairs.add(int);
+    }
+  }
+
+  return pairCount;
+
 }
