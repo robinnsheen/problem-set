194d1e5e310da1e37ac96863cdb42c2d7d53634e
robin <robinnsheen@berkeley.edu>
2022-05-31 01:10:09 -0700

    finished longest-fall

--- longest-fall.js
+++ longest-fall.js
@@ -1,3 +1,22 @@
-// add whatever parameters you deem necessary & write docstring
-function longestFall() {
+"use stict";
+
+/* Return the length of the longest consecutive decrease in ints */
+function longestFall(ints) {
+  if (ints.length === 0) return 0;
+
+  consecFall = 1;
+  longestConsecFall = 1;
+
+  for ( let i=0; i<ints.length-1; i++ ) {
+    if (ints[i+1] < ints[i]) {
+      consecFall++;
+      longestConsecFall = Math.max(longestConsecFall, consecFall);
+    } else {
+      consecFall = 1;
+    }
+
+  }
+
+  return longestConsecFall;
+
 }
