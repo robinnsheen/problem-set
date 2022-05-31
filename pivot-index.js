ae0bca9e92614855c71f52ba9f2781575c9ad52b
robin <robinnsheen@berkeley.edu>
2022-05-31 02:07:12 -0700

    pivot index finished

--- pivot-index.js
+++ pivot-index.js
@@ -1,3 +1,43 @@
-// add whatever parameters you deem necessary & write docstring
-function pivotIndex() {
+"use strict";
+
+/* Return smallest pivot index where sum of left equals sum of right */
+function pivotIndex(ints) {
+  console.log(ints);
+
+  let left = 0;
+  let right = ints.length-1;
+
+  let leftSum = 0;
+  let rightSum = 0;
+
+  const indexToSum = new Map();
+  let lowestPivot = ints.length;
+
+  while (left < ints.length-1) {
+
+    leftSum += ints[left];
+    rightSum += ints[right];
+
+    if (left < right) {
+      indexToSum.set(left, leftSum);
+      indexToSum.set(right, rightSum);
+    }
+
+    if (left > right) {
+
+      if (leftSum === indexToSum.get(left)) {
+        lowestPivot = Math.min(lowestPivot, left);
+      }
+      if (rightSum === indexToSum.get(right)) {
+        lowestPivot = Math.min(lowestPivot, right);
+      }
+    }
+
+    left++;
+    right--;
+  }
+
+  if (lowestPivot === ints.length) return -1;
+  return lowestPivot;
+
 }
